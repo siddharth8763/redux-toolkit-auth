1d44c3a4162442da61f7c3f2ef7a22c2ad441288
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2024-04-22 22:58:18 +0530

    Initial Commit

---
+++ src/register/Register.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { register } from "../store/reducers/auth";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("register", username, email, password);
+    dispatch(register({ email, username, password })).then((action) => {
+      localStorage.setItem("accessToken", action.payload.token);
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
